b7577419030625b705f195f7e8faffc8795df08f
Egor Blinov <e.blinov@fun-box.ru>
2014-12-08 11:18:00 +0300

    add memoization option

--- lib/service_factory.rb
+++ lib/service_factory.rb
@@ -1,34 +1,65 @@
 require "service_factory/version"
 
 module ServiceFactory
-  @blocks = {}
-
   def register(&block)
-    Builder.new(@blocks).instance_eval(&block)
+    @factory = Builder.new.build(&block)
   end
   module_function :register
 
   def self.method_missing(m, *args)
-    if @blocks.include?(m)
-      @blocks[m].call(*args)
+    if @factory.memoized_values.include?(m)
+      @factory.memoized_values[m]
+    elsif @factory.blocks.include?(m)
+      @factory.blocks[m].call(*args)
     else
       super
     end
   end
 
-  class Builder
-    def initialize(blocks)
+  class FactoryInstance
+    attr_reader :blocks, :memoized_values
+    def initialize(blocks, memoized_values)
       @blocks = blocks
+      @memoized_values = memoized_values
+    end
+  end
+
+  class Builder
+    def initialize
+      @blocks = {}
+      @memoized_values = {}
+      @memoization = false
+    end
+
+    def build(&block)
+      instance_eval(&block)
+      FactoryInstance.new(@blocks, @memoized_values)
     end
 
     def method_missing(m, *args, &block)
       if block_given?
-        @blocks[m] = block
+        define_from_block(m, args, &block)
       elsif args.first.is_a?(Class)
         cl = args.first
-        @blocks[m] = Proc.new { |*class_args| cl.new(*class_args) }
+        define_from_class(m, cl, &block)
       else
         super
+      end
+    end
+
+    def define_from_block(m, args, &block)
+      if @memoization
+        @memoized_values[m] = block.call
+      else
+        @blocks[m] = block
+      end
+    end
+
+    def define_from_class(m, cl, &block)
+      if @memoization
+        @memoized_values[m] = cl.new
+      else
+        @blocks[m] = Proc.new { |*class_args| cl.new(*class_args) }
       end
     end
 
@@ -36,6 +67,12 @@ module ServiceFactory
       if environments.map{|e| e.to_s}.include?(Rails.env)
         instance_eval(&block)
       end
+    end
+
+    def memoize(&block)
+      @memoization = true
+      instance_eval(&block)
+      @memoization = false
     end
   end
 end
